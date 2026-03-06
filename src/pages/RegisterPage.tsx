53caea581f588c52b16f016161a4b5d34311672d
sowmya <sowmya.t@everest.engineering>
2026-03-06 17:39:33 +0530

    Update the registration page to change the form data

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,22 +1,45 @@
 import '../styles/PageStyle.css'
 import React, { useState } from 'react';
+import { UserDataTypes } from '../types/UserDetails';
 // import { useNavigate } from 'react-router-dom';
 
 
-export const SignUp = () => {
-  const [firstName, setFirstName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [file, setFile] = useState<File | undefined>(undefined);
-  const [error, setError] = useState('');
 
-  const handleRegister = () => {
-    console.log('user register register:', { firstName,email });
-    if (!firstName || !email || !password) {
+export const SignUp = () => {
+ 
+
+  const [file, setFile] = useState<File | undefined>();
+  const [error, setError] = useState('');
+  const [userValues, setUserValues] = useState<UserDataTypes[]>([])
+  const [formData,setFormData] = useState<UserDataTypes>({
+    id:'',
+    name: '',
+    email: '',
+    password: ''
+  })
+  
+
+  const handleRegister = async (data:UserDataTypes) => {
+
+    console.log('coming the register data..', data);
+    try{
+    if (!data.name || !data.email || !data.password) {
       setError('Please fill in all fields');
-      return;
+      return alert("please enter the all fields");
+    }else{
+       console.log("formdata", formData)
+       await fetch(`http://localhost:3006/signUp`, {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ ...formData, id: formData.id }),
+       });
+       setUserValues([...userValues, { ...formData }]);
+    }
+    }catch{
+      alert("the user details not added to the db")
     }
   };
+
    function handleProfile(e: React.ChangeEvent<HTMLInputElement>) {
         if (e.target.files != null) {
             setFile(e.target.files[0]); 
@@ -31,18 +54,18 @@ export const SignUp = () => {
             <h3>Sign Up</h3>
             <label>Name:</label>
             <div className='name-container'>
-              <input className="first-name"type="text" placeholder="first name" value={firstName} autoFocus onChange={e => setFirstName(e.target.value)}/>
+              <input className="first-name"type="text" placeholder="name" value={formData.name} autoFocus onChange={e => setFormData({...formData,name:e.target.value})}/>
             </div>
             <label>Email:</label>
-            <input type="email" placeholder="Email" value={email} onChange={e=>setEmail(e.target.value)} />
+            <input type="email" placeholder="Email" value={formData.email} onChange={e=>setFormData({...formData,email:e.target.value})} />
             <label>Password:</label>
-            <input type="password" placeholder="password" value={password} onChange={e => setPassword(e.target.value)} />
+            <input type="password" placeholder="password" value={formData.password} onChange={e => setFormData({...formData,password:e.target.value})} />
             <label>Profile Image:</label>
             <div>
             <input type="file" onChange={()=>handleProfile} />
             <img src={fileSrc} alt="profile" />
             </div>
-            <button className="register-button"onClick={handleRegister}>Sign Up</button>
+            <button className="register-button"onClick={()=>{handleRegister(formData)}}>Sign Up</button>
              {error && <p className="error-text">{error}</p>}
             <p>Already have an account <a href="/login">SignUp</a></p>
         </div>
