15b7b79d99911a6818298844952197175fec374a
sowmya <sowmya.t@everest.engineering>
2026-03-07 01:14:07 +0530

    Update the login page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,63 +1,38 @@
 
-import { useState } from 'react';
-import { useEffect } from 'react';
-import  '../styles/PageStyle.css'
-import {useNavigate} from 'react-router-dom';
-
-
+import '../styles/PageStyle.css'
+import useBackend from '../hook/useBackend';
 
 export const Login = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
-    const navigate = useNavigate();
-   
 
-    useEffect(() => {
-    console.log('Login Component rendered');
-    }, []);
+    const { formData, setFormData, handleLogin, error } = useBackend()
+    return (
+        <div className='main'>
+            <h3>Fitness</h3>
+            <div className="form">
+                <h3>Login Here</h3>
+                <label>Username:</label>
+                <input
+                    type="email"
+                    placeholder="Email"
+                    value={formData.email}
+                    autoFocus
+                    onChange={e => {
+                        setFormData({ ...formData, email: e.target.value });
+                    }} />
 
-    const handleLogin = () => {
-    //     console.log(' Login attempt with:', { email, password });
-    //     const user = users.find(user => user.email === email && user.password === password);
-    //     if (user) {
-    //     login(user);
-    //     console.log('Login successful..',user.name);
-        navigate('/home');
-    //     } else {
-    //     console.log(' Login failed');
-        setError('Invalid email or password');
-    //     }
-    };
-
-    return(
-    <div className='main'>
-        <h3>Fitness</h3>
-        <div className="form">
-            <h3>Login Here</h3>
-            <label>Username:</label>
-            <input 
-              type="email" 
-              placeholder="Email"
-              value={email} 
-              autoFocus 
-              onChange={e=>{
-                setEmail(e.target.value);
-            } }/>
-            
-            <label>Password:</label>
-            <input 
-              type="password" 
-              placeholder="password"
-              value={password} 
-              onChange={e=>{
-                setPassword(e.target.value);
-            }} />
-            <button className="register-button"onClick={handleLogin}>Login</button>
-            {error && <p className="error-text">{error}</p>}
-            <p>Don't have an account <a href="/home">Sign In</a></p>
+                <label>Password:</label>
+                <input
+                    type="password"
+                    placeholder="password"
+                    value={formData.password}
+                    onChange={e => {
+                        setFormData({ ...formData, password: e.target.value });
+                    }} />
+                <button className="register-button" onClick={() => handleLogin(formData)}>Login</button>
+                {error && <p className="error-text">{error}</p>}
+                <p>Don't have an account <a href="/home">Sign In</a></p>
+            </div>
         </div>
-    </div>
     )
 }
 export default Login;
