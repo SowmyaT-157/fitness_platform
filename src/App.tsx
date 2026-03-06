ff1bb8b0ffe7d70604aedac968e54493a081abe2
sowmya <sowmya.t@everest.engineering>
2026-03-07 00:26:34 +0530

    Update the root file to add the path

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import SignUp from './pages/RegisterPage';
 import Login from './pages/LoginPage';
 import HomePage from './pages/HomePage';
+import Verify from './pages/verificationPage';
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
           <Route path="/" element={<SignUp/>}/>
-          <Route path="/login" element={<Login/>}/>
+          <Route path="/verify" element={<Verify/>}/>
+          <Route path="/signIn" element={<Login/>}/>
           <Route path="/home" element={<HomePage/>}/>
     </Routes>
     </BrowserRouter>
