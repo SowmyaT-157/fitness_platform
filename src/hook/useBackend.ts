af86c615946f77e44d18de18711357d01132805c
sowmya <sowmya.t@everest.engineering>
2026-03-07 01:12:02 +0530

    Update the hook to add the signIn logic

--- src/hook/useBackend.ts
+++ src/hook/useBackend.ts
@@ -1,9 +1,10 @@
 
 import { useState } from "react";
-import { UserDataTypes, verifyDataTypes } from "../types/UserDetails";
+import { signInDataTypes, UserDataTypes, verifyDataTypes } from "../types/UserDetails";
+import { useNavigate } from "react-router-dom";
 
 const useBackend = () =>{
-    
+      const navigate = useNavigate();
       const [error, setError] = useState('');
       const [userValues, setUserValues] = useState<UserDataTypes[]>([])
       const [formData,setFormData] = useState<UserDataTypes>({
@@ -31,6 +32,7 @@ const useBackend = () =>{
               body: JSON.stringify({ ...formData, id: formData.id }),
            });
            setUserValues([...userValues, { ...formData }]);
+           navigate('/verify');
         }
         }catch{
           alert("the user details not added to the db")
@@ -49,17 +51,40 @@ const useBackend = () =>{
                     headers: { 'Content-Type': 'application/json' },
                     body: JSON.stringify({ email: data.email, code: data.code  }),
                });
+              
                if(!responseData){
                 return "verification failed"
                }else{
                 alert("successfully verified the email")
+                navigate('/signIn');
                 return "successfully verified"
+                
                }
             }
         }catch{
             alert("email and verification code check once")
         }
-        };
+    };
+    const handleLogin = async(data:signInDataTypes) =>{
+      try{
+        if (!data.email || !data.password) {
+          setError('Please fill in all fields');
+          return alert("please enter the all fields");
+        }else{
+           console.log("formdata", formData)
+           await fetch(`http://localhost:3006/signIn`, {
+              method: 'POST',
+              headers: { 'Content-Type': 'application/json' },
+              body: JSON.stringify({ ...formData, id: formData.id }),
+           });
+          //  setUserValues([...userValues, { ...formData }]);
+           navigate('/home');
+        }
+        }catch{
+          alert("the email and password not matched to user")
+        }
+
+    }
     
     return{
        handleRegister,
@@ -69,8 +94,8 @@ const useBackend = () =>{
        setError,
        verifyForm,
        setVerifyForm,
-       handleVerification
-
+       handleVerification,
+       handleLogin
     }
 }
 export default useBackend;
