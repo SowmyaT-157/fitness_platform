a195f3085fdeb8120599bf4fd75d01e41f7a1ca6
sowmya <sowmya.t@everest.engineering>
2026-03-08 23:14:34 +0530

    Update the home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,18 @@
+import useBackend from "../hook/useBackend";
 
-const HomePage = () =>{
-    
-    return(
+
+
+const HomePage = () => {
+    const { currentUser } = useBackend()
+    console.log("image comming", currentUser)
+    return (
         <div>
-           <h2> Welcome to home page</h2>
-           <h5>UserName:</h5>
-           <h5>Password:</h5>
-           <p>image:</p>
-        </div> 
+            <h2>Welcome to home page</h2>
+            <h5>user name: {currentUser.name}</h5>
+            <h5>email: {currentUser.email}</h5>
+            <p>profile image:</p>
+            <img src={currentUser.image} alt="profile" style={{ width: '100px' }} />
+        </div>
     )
 }
 export default HomePage;
