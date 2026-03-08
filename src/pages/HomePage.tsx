4618b15a0362b7dd0261cd6e5342517d1e555da8
sowmya <sowmya.t@everest.engineering>
2026-03-08 11:23:32 +0530

    Update the home page to add fields

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,13 @@
 
 const HomePage = () =>{
+    
     return(
-        <h2> Welcome to home page</h2>
+        <div>
+           <h2> Welcome to home page</h2>
+           <h5>UserName:</h5>
+           <h5>Password:</h5>
+           <p>image:</p>
+        </div> 
     )
 }
 export default HomePage;
