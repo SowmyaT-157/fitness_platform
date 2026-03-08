635eb9d24311b7d110d21950445fe2ba8e9fee63
sowmya <sowmya.t@everest.engineering>
2026-03-08 23:04:09 +0530

    Update the main file to add the provider

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,19 @@ import SignUp from './pages/RegisterPage';
 import Login from './pages/LoginPage';
 import HomePage from './pages/HomePage';
 import Verify from './pages/verificationPage';
+import { BackendProvider } from './hook/useBackend';
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-          <Route path="/" element={<SignUp/>}/>
-          <Route path="/verify" element={<Verify/>}/>
-          <Route path="/signIn" element={<Login/>}/>
-          <Route path="/home" element={<HomePage/>}/>
-    </Routes>
+    <BackendProvider>
+      <Routes>
+            <Route path="/" element={<SignUp/>}/>
+            <Route path="/verify" element={<Verify/>}/>
+            <Route path="/signIn" element={<Login/>}/>
+            <Route path="/home" element={<HomePage/>}/>
+      </Routes>
+    </BackendProvider>
     </BrowserRouter>
   );
 }
